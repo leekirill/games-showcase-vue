e4e0e35a392e95f93f12ee9521d9a065018aeb8e
ваше имя <cyril.lee1997@gmail.com>
2023-02-01 21:03:38 +0200

    .

---
+++ src/components/AppHome.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="d,i in counts" :key={i}>{{ d }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counts: [1,3,4]
+        }
+    },
+}
+</script>
